e2c57ff8aed077b5bc4ba65642bcbabbaa589f7c
zmeu` <tsraduu@gmail.com>
2023-08-02 18:31:15 +0100

    User choice
    Added event listner for user's choice and working on computer's choice

--- assets/main.js
+++ assets/main.js
@@ -2,16 +2,31 @@
 
 let playingUser;
 const restartButton = document.getElementById("restartBtn");
+const choices = document.querySelectorAll("#rock, #paper, #scissors");
 
 // Todo: Ask for user name when the page loads, only runs when the page loads
 
-window.addEventListener("load", () => {
-  playingUser = prompt("And who am I playing against ?", "");
-});
-
-//Todo: Computer's selectioni
+// window.addEventListener("load", () => {
+//   playingUser = prompt("And who am I playing against ?", "");
+// });
 
 //Todo: User selection
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.id;
+    console.log(`${userChoice} was clicked`);
+  });
+});
+
+//Todo: Computer's selection
+
+const coChoices = ["rock", "paper", "scissors"];
+
+function getRandomChoices() {
+  const randomIndex = Math.floor(Math.random() * coChoices.length);
+  return randomIndex;
+}
 
 //Todo: The game runs once
 
